76d20a2f3f633b270978e434004ba2642c9f6726
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-07-10 05:21:07 +0530

    Add Modified 2202A.cpp

--- 2202A.cpp
+++ 2202A.cpp
@@ -6,7 +6,35 @@ using namespace std;
 
 void solve()
 {
+    int x,y;
+    cin >> x >> y;
+
+    bool flag1 = false;
+
+    if((x-(2*y)) % 3 == 0)
+    flag1 =true;
+
+    bool flag2 = false;
+
+    if((y>=0) && (x>=2*y))
+    {
+        flag2 = true;
+    }
+
+    if ((y < 0) && (x >= (-4*y)))
+    {
+        flag2 = true;
+    }
     
+
+    if(flag1 && flag2)
+    {
+        cout << "yes" << "\n";
+    }
+    else
+    {
+        cout << "no" << "\n";
+    }
 }
 
 int main()
@@ -24,3 +52,5 @@ int main()
 
     return 0;
 }
+
+// SAW THE HINT
