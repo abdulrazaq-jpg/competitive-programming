080b4f894d633579a09ae5f1861b63df3c759dce
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-07-28 06:39:45 +0530

    Add Contents Of 28-7-26

--- JULY-26/WEEK-4/26-7-26/2170A.cpp
+++ JULY-26/WEEK-4/26-7-26/2170A.cpp
@@ -9,6 +9,19 @@ void solve()
     int n;
     cin >> n;
 
+    int arr[101][101];
+
+    int x = 1;
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            arr[i][j] = x;
+            x++;
+        }
+    }
+
     if(n==1)
     {
         cout << 1 << "\n";
@@ -30,6 +43,14 @@ void solve()
     if(n==4)
     {
         cout << 56 << "\n";
+        return;
+    }
+
+    if(n > 4)
+    {
+        int ans = arr[n-1][n-2] + arr[n-1][n-1] + arr[n-1][n] + arr[n-2][n-1] + arr[n][n-1];
+
+        cout << ans << "\n";
         return;
     }
 }
