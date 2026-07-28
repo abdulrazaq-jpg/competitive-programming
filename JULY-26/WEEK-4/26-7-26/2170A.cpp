41536088cdf2a7123b69e5887eecd94a51eed059
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-07-28 17:01:22 +0530

    Add contents of 28-7-26

--- JULY-26/WEEK-4/26-7-26/2170A.cpp
+++ JULY-26/WEEK-4/26-7-26/2170A.cpp
@@ -70,3 +70,33 @@ int main()
 
     return 0;
 }
+
+
+/*
+#include <bits/stdc++.h>
+ using namespace std;
+ int dx[] = {-1, 0, 1, 0};int dy[] = {0, -1, 0, 1};
+ int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        auto get = [&](int x, int y) {
+            if (x >= 0 && x < n && y >= 0 && y < n) return x * n + y + 1;
+            return 0;
+        };
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int cur = get(i, j);
+                for (int d = 0; d < 4; ++d) cur += get(i + dx[d], j + dy[d]);
+                ans = max(ans, cur);
+            }
+        }
+        cout << ans << '\n';
+    }}
+
+What is this code . Explain me
+
+*/
