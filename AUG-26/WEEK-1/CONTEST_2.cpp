85cc603da22417f10ba1b1883456d7a71cf20390
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-02 23:13:48 +0530

    Add Content Of 2-8-26

--- AUG-26/WEEK-1/CONTEST_2.cpp
+++ AUG-26/WEEK-1/CONTEST_2.cpp
@@ -1,131 +1,51 @@
-// Merge to Match
-
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
-void solve()
-{
+void solve() {
     int n, m;
     cin >> n >> m;
 
-    vector<int> a(n);
-    vector<int> b(m);
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
+    vector<int> a(n), b(m);
+    for (int &x : a) {
+        cin >> x;
+    }
+    for (int &x : b) {
+        cin >> x;
     }
 
-    for (int i = 0; i < m; i++)
-    {
-        cin >> b[i];
-    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
 
-    if(n == 2 && m == 1)
-    {
-        int k = max(a[0],a[1]);
-        int p = min(a[0],a[1]);
-
-        if(b[0] >= p && b[0] <= k)
-        {
-            cout << "YES\n";
-            return;
-        }
-        else
-        {
-            cout << "NO\n";
-            return;
-        }
-    }
-
-    int diff = n - m;
-
-    bool flag = true;
-
-    if (diff < 0)
-    {
-        flag = false;
-    }
-    else if (diff == 0)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            if (a[i] != b[i])
-            {
-                flag = false;
-                break;
-            }
-        }
-    }
-    else if (diff == 1)
-    {
-        flag = false;
-    }
-    else
-    {
-        int max_a = a[0];
-        int min_a = a[0];
-
-        for (int i = 0; i < n; i++)
-        {
-            int x = a[i];
-
-            if (x > max_a)
-            {
-                max_a = x;
-            }
-
-            if (x < min_a)
-            {
-                min_a = x;
-            }
-        }
-
-        int max_b = b[0];
-        int min_b = b[0];
-
-        for (int i = 0; i < m; i++)
-        {
-            int y = b[i];
-
-            if (y > max_b)
-            {
-                max_b = y;
-            }
-
-            if (y < min_b)
-            {
-                min_b = y;
-            }
-        }
-
-        if ((max_a < max_b) || (min_a > min_b))
-        {
-            flag = false;
-        }
-    }
-
-    if (flag)
-    {
-        cout << "YES\n";
-    }
-    else
-    {
+    // Each element in 'b' needs at least 2 elements from 'a' (one strictly smaller and one strictly larger)
+    if (n < 2 * m) {
         cout << "NO\n";
+        return;
+    }
+
+    int i = 0;
+    // For every b[i], check if a[i] < b[i] and b[i] < a[n - m + i]
+    while (i < m && a[i] < b[i] && b[i] < a[n - m + i]) {
+        ++i;
+    }
+
+    if (i < m) {
+        cout << "NO\n";
+    } else {
+        cout << "YES\n";
     }
 }
 
-int main()
-{
+int main() {
+    // Fast I/O
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t;
-    cin >> t;
-
-    while (t--)
-    {
+    int T;
+    cin >> T;
+    while (T--) {
         solve();
     }
 
