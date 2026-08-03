35feff2255deac88f6bd0f8a9e3ed4f3ccd870c4
Abdul Razaq <abdulrazaqramdurg006@gmail.com>
2026-08-03 22:48:56 +0530

    Add Content Of 3-8-26

---
+++ AUG-26/WEEK-1/3-8-26/2155A.cpp
@@ -0,0 +1,29 @@
+// El fucho
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    cout << 2*(n-1) << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
